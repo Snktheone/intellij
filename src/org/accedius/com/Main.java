508ab3eabb54be57ca8884f03bbfedafc05df488
Accedius <sarang@accedius.com>
2016-11-16 21:11:54 +0000

    Initial commit

---
+++ src/org/accedius/com/Main.java
@@ -0,0 +1,22 @@
+package org.accedius.com;
+
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int val1 = 100 ;
+        int val2 = 200 ;
+        int result =  add(val1 , val2);
+
+        System.out.print(result);
+    }
+
+    private static int add(int num1, int num2)
+    {
+        return  num1 + num2;
+
+
+    }
+}
